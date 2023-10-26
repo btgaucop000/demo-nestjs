a3f280bcd10f417dcb5cfa3ba2fae2c9053c6e69
TienTB <tabaotien1997@gmail.com>
2023-10-26 23:11:56 +0700

    feat: implement jwt token and refresh token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,31 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { AuthService } from './auth.service';
 import { User } from 'src/user/entities/user.entity';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('register')
   register(@Body() registerUser: RegisterUserDto): Promise<User> {
-    console.log('register api', registerUser);
     return this.authService.register(registerUser);
+  }
+
+  @Post('login')
+  @UsePipes(new ValidationPipe())
+  login(@Body() loginUser: LoginUserDto): Promise<any> {
+    return this.authService.login(loginUser);
+  }
+
+  @Post('refresh-token')
+  refreshToken(@Body() { refresh_token }): Promise<any> {
+    return this.authService.refreshToken(refresh_token);
   }
 }
