df3da21c04b919b12e93954c9b86c6b3901cc9f4
TienTB <tabaotien1997@gmail.com>
2023-11-07 21:50:38 +0700

    feat: apply pagination + search

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,6 +7,7 @@ import {
   Body,
   Put,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { User } from './entities/user.entity';
 import { UserService } from './user.service';
@@ -14,6 +15,7 @@ import { AuthGuard } from 'src/auth/auth.guard';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { DeleteResult, UpdateResult } from 'typeorm';
+import { FilterUserDto } from './dto/filter-user.dto';
 
 @Controller('users')
 export class UserController {
@@ -21,8 +23,9 @@ export class UserController {
 
   @UseGuards(AuthGuard)
   @Get()
-  findAll(): Promise<User[]> {
-    return this.userService.findAll();
+  findAll(@Query() query: FilterUserDto): Promise<any> {
+    console.log(query);
+    return this.userService.findAll(query);
   }
 
   @UseGuards(AuthGuard)
